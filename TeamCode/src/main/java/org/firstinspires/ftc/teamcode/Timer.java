7e9dbed51dab986aadd98eab72fd3efe0b5b1ac5
aircr <18bt2221@medinabees.org>
2017-11-20 14:35:41 -0500

    Commenting the Timer Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Timer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Timer.java
@@ -1,30 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
 /**
- * Created by aircr on 11/20/2017.
+ * Created by Brian Towne on 11/20/2017.
  */
 
 public class Timer extends Thread
 {
-    public int time;
+    /**
+     * !!! This classes uses Multithreading. !!!
+     *
+     * This class is used to create a timer for the robot.
+     * It will push this task onto another thread so it will not interfere with the main functions
+     *
+     * Check VuforiaNavigation for use.
+     */
 
-    public boolean isDone;
+    public int time; // The amount of time to elapse
 
+    public boolean isDone; // To check if the timer is done
+
+    // Constructor
     public Timer (int millis)
     {
         time = millis;
         isDone = false;
     }
 
+    // This is what will run when Timer.start() is ran.
     @Override
     public void run()
     {
-        isDone = false;
+        isDone = false; // Make sure the timer is not done
+
         try {
-            Thread.sleep(time);
+            Thread.sleep(time); // Make the thread sleep for the allotted time.
         } catch ( InterruptedException e) {
-            Thread.currentThread().interrupt();
+            Thread.currentThread().interrupt(); // Make sure the thread is interrupted for the full time.
         }
-        isDone = true;
+        isDone = true; // Make the timer done.
     }
 }
